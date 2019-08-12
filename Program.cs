514f1b87e7939db1a5203732882cb7018dfc76cf
Atte Collan <atte.collan@gmail.com>
2019-08-12 12:44:47 +0300

    t3

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ namespace DependencyInjectionExercise1
 {
     class Program
     {
+        // test change . . .
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
